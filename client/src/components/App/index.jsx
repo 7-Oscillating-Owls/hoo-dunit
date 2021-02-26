8cd59a9eea916e0dbda738db81914dd956bbd47e
Phillip Ho <philasan@gmail.com>
2021-02-25 20:18:23 -0800

    separate out babel config from webpack

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => (
   <div className="app">
-    <h1 className="title">Hello Owls</h1>
+    <h1 className="title">Hello Owls!</h1>
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,6 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader', // transpiles React from ES6 to ES5
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
         },
       },
     ],
