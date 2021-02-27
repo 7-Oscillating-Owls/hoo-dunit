91698146d71c24751fd80e00cbd2c4d58e163867
Phillip Ho <philasan@gmail.com>
2021-02-27 12:37:54 -0800

    stub for RelatedProductList

---
+++ client/src/components/RelatedProductsList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './RelatedProductsList.css';
+
+const RelatedProductsList = () => (
+  <div className={styles.relatedProductsList}>
+    cards go here
+  </div>
+);
+
+export default RelatedProductsList;
