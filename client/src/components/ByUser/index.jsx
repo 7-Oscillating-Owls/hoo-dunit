9788479b3585361fce3f1b3d89e1abb07e8e4f1c
Ankur Tandan <tandan@gmail.com>
2021-02-28 15:01:41 -0800

    implemented byUser component

---
+++ client/src/components/ByUser/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ByUser.css';
+
+const ByUser = () => (
+  <small className={styles.byuser}>by User1337, January 1, 2020</small>
+);
+
+export default ByUser;
