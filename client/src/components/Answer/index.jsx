da614d1b1f5c9d8ba47051656d7695a940c41b5e
Ankur Tandan <tandan@gmail.com>
2021-02-27 20:28:54 -0800

    implemented Answer component

---
+++ client/src/components/Answer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Answer.css';
+
+const Answer = () => (
+  <div className={styles.answer}>
+    <h4>A:</h4>
+    <p>Alias libero voluptas adipisci et quam iure vel</p>
+  </div>
+);
+
+export default Answer;
