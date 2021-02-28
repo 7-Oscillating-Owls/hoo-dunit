e69f166961b058aff4b59f44fe545b56ae6de982
Phillip Ho <philasan@gmail.com>
2021-02-27 23:34:03 -0800

    added mock data, passed data to sub components

--- client/src/components/RelatedProducts/index.jsx
+++ client/src/components/RelatedProducts/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
 import RelatedProductsList from '../RelatedProductsList';
+import {
+  relatedProductsDetails,
+  stylesByProductId,
+} from '../../../../data/products';
 
 import styles from './RelatedProducts.css';
 
 const RelatedProducts = () => (
   <section className={styles.relatedProducts}>
     <h3 className={styles.listTitle}>Related Products</h3>
-    <RelatedProductsList />
+    <RelatedProductsList
+      relatedProducts={relatedProductsDetails}
+      stylesByProductId={stylesByProductId}
+    />
+
     <h3 className={styles.listTitle}>Your Outfit</h3>
-    <RelatedProductsList />
+    <RelatedProductsList
+      relatedProducts={relatedProductsDetails}
+      stylesByProductId={stylesByProductId}
+    />
   </section>
 );
 
