6d33648d1cac332da5610bfbe650efa86657e432
Kanchan Chauhan <kanchan827@gmail.com>
2021-02-28 13:39:54 -0800

    implemented basic funtionality for Image Gallery using dummy data

--- client/src/components/Overview/index.jsx
+++ client/src/components/Overview/index.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styles from './Overview.css';
+import dummyData from '../../../../data/styles';
+import ImageGallery from '../ImageGallery';
 
-const Overview = () => (
-  <div className={styles.overview}>
-    <div className={styles.imageGallery}>Image Gallery</div>
-    <div className={styles.productInformation}>Product information</div>
-    <div className={styles.styleSelector}>Style Selector</div>
-    <div className={styles.addToCart}>Add to Cart</div>
-    <div className={styles.productDescription}>Product Description</div>
-  </div>
-);
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allStyles: dummyData.results,
+    };
+  }
+
+  render() {
+    const { allStyles } = this.state;
+    let images;
+    allStyles.forEach((style) => {
+      if (style['default?'] === true) {
+        images = style.photos;
+      }
+    });
+
+    return (
+      <div className={styles.overview}>
+        <div className={styles.imageGallery}><ImageGallery images={images} /></div>
+        <div className={styles.productInformation}>Product information</div>
+        <div className={styles.styleSelector}>Style Selector</div>
+        <div className={styles.addToCart}>Add to Cart</div>
+        <div className={styles.productDescription}>Product Description</div>
+      </div>
+    );
+  }
+}
 
 export default Overview;
