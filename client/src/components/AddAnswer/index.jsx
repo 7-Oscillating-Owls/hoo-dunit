d37609e1547edb30105ad59fbac9a98ab14e91f6
Ankur Tandan <tandan@gmail.com>
2021-02-28 14:06:41 -0800

    implemented AddAnswer component

---
+++ client/src/components/AddAnswer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './AddAnswer.css';
+
+const AddAnswer = () => (
+  <small className={styles.AddAnswer}><a>Add Answer</a></small>
+);
+
+export default AddAnswer;
+
