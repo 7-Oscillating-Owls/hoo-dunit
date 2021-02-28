8fd3ea65b7c6ea3543cd0c76988edce69f32a13a
Monica Bui <monicabbui@gmail.com>
2021-02-27 17:25:27 -0800

    Create reviews and ratings file structure

---
+++ client/src/components/ReviewsAddForm/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ReviewsAddForm.css';
+
+const ReviewsAddForm = () => (
+  <div className={styles.reviewsAddForm}>
+    <div>Need to add tile components here</div>
+  </div>
+);
+
+export default ReviewsAddForm;
