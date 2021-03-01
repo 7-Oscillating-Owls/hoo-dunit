1e7ecd0aaf61796815082484be11512f16216a02
Kanchan Chauhan <kanchan827@gmail.com>
2021-02-28 19:33:57 -0800

    rendered sizes as option in select a size button

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import styles from './Cart.css';
 
-const Cart = () => (
-  <div className={styles.selectContainer}>
-    <form>
-      <select name="size">
-        <option value="">SELECT SIZE</option>
-      </select>
-      <select name="quantity">
-        <option value="">1</option>
-      </select>
-      <br />
-      <input type="submit" value="ADD TO BAG" />
-    </form>
-  </div>
-);
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      itemSelected: "",
+    };
+  }
+
+  render() {
+    const { skus } = this.props;
+    const sizes = Object.keys(skus);
+
+    return (
+      <div className={styles.selectContainer}>
+        <form>
+          <select name="size">
+            {
+              sizes.map((item, index) => (
+                <option value={skus[item].size} key={index}>{skus[item].size}</option>
+              )
+              )}
+
+          </select>
+          <select name="quantity">
+            <option value="">1</option>
+          </select>
+          <br />
+          <input type="submit" value="ADD TO BAG" />
+        </form>
+      </div>
+    );
+  }
+}
+
 
 export default Cart;
