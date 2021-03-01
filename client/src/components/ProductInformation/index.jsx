f845e6a06402de826bac19400a5e05b1af7b324d
Kanchan Chauhan <kanchan827@gmail.com>
2021-02-28 23:33:06 -0800

    rendered relevent product information for a specific style-id

--- client/src/components/ProductInformation/index.jsx
+++ client/src/components/ProductInformation/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import styles from './styles.css'
+import PropTypes from 'prop-types';
+import styles from './styles.css';
 
-const ProductInformation = ({productInfo, }) => (
-  <div className={styles.productInfo}>
-    <div className={styles.ratings}>Star Ratings</div>
-    <div className={styles.category}>Category</div>
-    <div className={styles.name}>Name</div>
-    <div className={styles.price}>Price</div>
-  </div>
-);
+const ProductInformation = ({ productInfo, originalPrice, salePrice }) => {
+  const currentPrice = salePrice === null ? originalPrice
+    : (
+      <span className={styles.originalPrice}>
+        {originalPrice}
+      </span>
+    );
+
+  return (
+    <div className={styles.productInfo}>
+      <div className={styles.ratings}>Star Ratings</div>
+      <div className={styles.category}>{productInfo.category}</div>
+      <div className={styles.name}>{productInfo.name}</div>
+      <div className={styles.salePrice}>
+        $
+        {salePrice}
+      </div>
+      <div className={styles.price}>
+        $
+        {currentPrice}
+      </div>
+    </div>
+  );
+};
+
+ProductInformation.propTypes = {
+  productInfo: PropTypes.objectOf(PropTypes.number).isRequired,
+  originalPrice: PropTypes.string.isRequired,
+  salePrice: PropTypes.number.isRequired,
+};
 
 export default ProductInformation;
