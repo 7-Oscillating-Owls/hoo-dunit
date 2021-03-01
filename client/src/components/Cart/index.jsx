a80b58a85f001c09a82d2dee5ab03eca3dcba76b
Kanchan Chauhan <kanchan827@gmail.com>
2021-02-28 18:18:08 -0800

    added select size button

---
+++ client/src/components/Cart/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => (
+  <div className="select-container">
+    <select name="size">
+      <option value="" disabled selected>SELECT SIZE</option>
+    </select>
+  </div>
+);
+
+export default Cart;
--- client/src/components/Overview/index.jsx
+++ client/src/components/Overview/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Overview.css';
 import dummyData from '../../../../data/styles';
 import ImageGallery from '../ImageGallery';
+import Cart from '../Cart';
 
 class Overview extends React.Component {
   constructor(props) {
@@ -26,7 +27,7 @@ class Overview extends React.Component {
         <div className={styles.imageGallery}><ImageGallery images={images} /></div>
         <div className={styles.productInformation}>Product information</div>
         <div className={styles.styleSelector}>Style Selector</div>
-        <div className={styles.addToCart}>Add to Cart</div>
+        <div className={styles.addToCart}><Cart /></div>
         <div className={styles.productDescription}>Product Description</div>
       </div>
     );
