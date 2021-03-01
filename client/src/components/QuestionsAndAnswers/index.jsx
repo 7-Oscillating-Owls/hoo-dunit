bf56c7c60d14f0061932cd304f02a66da715c3a2
Ankur Tandan <tandan@gmail.com>
2021-03-01 09:09:00 -0800

    begin reorganization of divs in questionsandanswers component

--- client/src/components/QuestionsAndAnswers/index.jsx
+++ client/src/components/QuestionsAndAnswers/index.jsx
@@ -4,27 +4,42 @@ import Question from '../Question';
 import Answer from '../Answer';
 import Helpful from '../Helpful';
 import AddAnswer from '../AddAnswer';
-
-
 import AnswerHelpful from '../AnswerHelpful';
 import ByUser from '../ByUser';
 import Report from '../Report';
 import LoadMoreAnswers from '../LoadMoreAnswers';
-
 import QATitle from '../QATitle';
+
 const QuestionsAndAnswers = () => (
 
   <div className={styles.qa}>
     <div classNames={styles.qatitle}><QATitle /></div>
     <div className={styles.searchBar}>SearchBar</div>
-    <div className={styles.helpful}><Helpful /></div>
-    <div className={styles.addanswer}><AddAnswer /></div>
+
     <div className={styles.qaContainer}>
-      <div className={styles.questionList}><Question /></div>
+      <div className={styles.questionList}>
+        <Question />
+        <div className={styles.helpful}><Helpful /></div>
+        <div className={styles.addanswer}><AddAnswer /></div>
+      </div>
       <div className={styles.byuser}><ByUser /></div>
-      <div className={styles.answerList}><Answer /></div>
-      <div className={styles.answerhelpful}><AnswerHelpful /></div>
-      <div className={styles.report}><Report /></div>
+      <div className={styles.answerList}>
+        <Answer />
+        <div className={styles.answerhelpful}><AnswerHelpful /></div>
+        <div className={styles.report}><Report /></div>
+      </div>
+      <div className={styles.qaContainer}>
+        <div className={styles.questionList}>
+          <Question />
+          <div className={styles.helpful}><Helpful /></div>
+          <div className={styles.addanswer}><AddAnswer /></div>
+        </div>
+      </div>
+      <div className={styles.answerList}>
+        <Answer />
+        <div className={styles.answerhelpful}><AnswerHelpful /></div>
+        <div className={styles.report}><Report /></div>
+      </div>
       <div className={styles.loadmoreanswers}><LoadMoreAnswers /></div>
     </div>
     <div className={styles.buttonContainer}>
