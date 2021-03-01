86100deaf2e8dcbaaac7c3d816854df2029f0f82
Kanchan Chauhan <kanchan827@gmail.com>
2021-03-01 12:11:22 -0800

    rendered all available styles based on style id

--- client/src/components/StyleSelector/index.jsx
+++ client/src/components/StyleSelector/index.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './StyleSelector.css';
 
-const StyleSelector = ({ images }) => (
+const StyleSelector = ({ allStyles }) => (
   <div className={styles.container}>
-    {images.map((item) => (
-      <div key={item.url}>
-        <img className={styles.item} src={item.thumbnail_url} alt="thumbnail" />
+    {allStyles.map((style) => (
+      <div key={style.photos[0].url}>
+        <img className={styles.item} src={style.photos[0].thumbnail_url} alt="thumbnail" />
       </div>
     ))}
   </div>
 );
 
 StyleSelector.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  allStyles: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default StyleSelector;
