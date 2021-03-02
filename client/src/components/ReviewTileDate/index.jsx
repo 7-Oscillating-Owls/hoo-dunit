ee3a588ae47093fdcd491c0ce363f10d625bd055
Monica Bui <monicabbui@gmail.com>
2021-03-01 16:54:30 -0800

    Refactored so that date subcomponent is separate file

---
+++ client/src/components/ReviewTileDate/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ReviewTileDate = (props) => {
+  const { date } = props;
+  const monthOptions = ['January ', 'February ', 'March ', 'April ', 'May ', 'June ', 'July ', 'August ', 'September ', 'October ', 'November ', 'December '];
+  const newDate = new Date(date);
+  const monthNumber = newDate.getMonth();
+  const month = monthOptions[monthNumber];
+  const day = newDate.getDate();
+  const year = newDate.getFullYear();
+  const formattedDate = `${month} ${day}, ${year}`;
+  return (
+    <div>{formattedDate}</div>
+  );
+};
+
+export default ReviewTileDate;
--- client/src/components/ReviewTiles/index.jsx
+++ client/src/components/ReviewTiles/index.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReviewTileStars from '../ReviewTileStars';
+import ReviewTileDate from '../ReviewTileDate';
 import styles from './ReviewTiles.css';
 
 const ReviewTiles = ({ review }) => {
   const { rating, reviewer_name, date, summary, body, helpfulness } = review;
-  const monthOptions = ['January ', 'February ', 'March ', 'April ', 'May ', 'June ', 'July ', 'August ', 'September ', 'October ', 'November ', 'December '];
-  const newDate = new Date(date);
-  const monthNumber = newDate.getMonth();
-  const month = monthOptions[monthNumber];
-  const day = newDate.getDate();
-  const year = newDate.getFullYear();
-  const formattedDate = `${month} ${day}, ${year}`;
   return (
     <div className={styles.reviewTiles}>
       <div>
         <ReviewTileStars ratings={rating} />
       </div>
       <div>{reviewer_name}</div>
-      <div>{formattedDate}</div>
+      <div>
+        <ReviewTileDate date={date} />
+      </div>
       <div>{summary}</div>
       <div>{body}</div>
       <div>
