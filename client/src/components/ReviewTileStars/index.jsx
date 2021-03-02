3d33b4830e8d1f5928146aec56997a750f902deb
Monica Bui <monicabbui@gmail.com>
2021-03-01 16:46:54 -0800

    Implemented tile stars so that empty stars are displayed - still need to handle half, 1/4, or 3/4 stars

--- client/src/components/ReviewTileStars/index.jsx
+++ client/src/components/ReviewTileStars/index.jsx
@@ -4,9 +4,11 @@ import { FaStar, FaRegStar, FaStarHalfAlt } from 'react-icons/fa';
 const ReviewTileStars = (props) => {
   const { ratings } = props;
   const numberOfStars = Math.floor(ratings);
+  const remainingStars = 5 - numberOfStars;
   return (
     <div>
       {[...Array(numberOfStars)].map((star) => <FaStar size={10} key={star} />)}
+      {[...Array(remainingStars)].map((star) => <FaRegStar size={10} key={star} />)}
     </div>
   );
 };
