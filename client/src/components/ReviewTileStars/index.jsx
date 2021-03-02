47d28b25a2a22a3d5783524d7f3a2d7d11c3f2e9
Monica Bui <monicabbui@gmail.com>
2021-03-01 19:06:02 -0800

    Implemented functional stars for both overview and tiles

--- client/src/components/ReviewTileStars/index.jsx
+++ client/src/components/ReviewTileStars/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { FaStar, FaRegStar, FaStarHalfAlt } from 'react-icons/fa';
+import { FaStar, FaRegStar } from 'react-icons/fa';
 
 const ReviewTileStars = (props) => {
   const { ratings } = props;
-  const numberOfStars = Math.floor(ratings);
-  const remainingStars = 5 - numberOfStars;
+  const remainingStars = 5 - ratings;
   return (
     <div>
-      {[...Array(numberOfStars)].map((star) => <FaStar size={10} key={star} />)}
+      {[...Array(ratings)].map((star) => <FaStar size={10} key={star} />)}
       {[...Array(remainingStars)].map((star) => <FaRegStar size={10} key={star} />)}
     </div>
   );
