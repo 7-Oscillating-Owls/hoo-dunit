1e2f263601ebb9d179cc083fb15cd554122eb626
Ankur Tandan <tandan@gmail.com>
2021-03-02 13:00:46 -0800

    added button

--- client/src/components/MoreAnsweredQuestions/index.jsx
+++ client/src/components/MoreAnsweredQuestions/index.jsx
@@ -1,3 +1,13 @@
-import React from 'react'
-import styles from './MoreAnsweredQuestions.css'
+import React from 'react';
+import styles from './MoreAnsweredQuestions.css';
 
+const MoreAnsweredQuestions = () => (
+  <button
+    className={styles.MoreAnsweredQuestions}
+    type="button"
+  >
+    MORE ANSWERED QUESTIONS
+  </button>
+);
+
+export default MoreAnsweredQuestions;
