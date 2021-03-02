00aa287e87683a5f6173f5b1293df7e6c149b113
Ankur Tandan <tandan@gmail.com>
2021-03-02 15:22:33 -0800

    added small tags for error for form validation

--- client/src/components/QuestionModal/index.jsx
+++ client/src/components/QuestionModal/index.jsx
@@ -8,15 +8,25 @@ class QuestionModal extends React.Component {
       question: '',
       nickname: '',
       email: '',
-      errors: {}
+      errors: {
+        question: '',
+        nickname: '',
+        email: ''
+      }
 
     };
     this.handleSubmitClick = this.handleSubmitClick.bind(this);
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.validate = this.validate.bind(this);
+  }
+  validate() {
+    console.log('validating')
+
   }
 
   handleSubmitClick(event) {
     event.preventDefault();
+    this.validate()
    this.props.modalclose();
   }
 
@@ -34,18 +44,21 @@ class QuestionModal extends React.Component {
             placeholder="Enter your question"
             maxLength="1000"
           />
+          <small>please enter a question</small>
           <input
             type="text"
             name="nickname"
             placeholder="Example: jackson11!"
             maxLength="60"
           />
+          <small>please enter a nickname</small>
           <input
             type="email"
             name="email"
             placeholder="enter email"
             maxLength="60"
           />
+          <small>please enter email in correct format</small>
           <small>For authentication reasons, you will not be emailed</small>
         </form>
         <button type="button" onClick={this.handleSubmitClick} className={styles.buttonModalForm}>Submit</button>
