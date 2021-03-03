052750a7a1c3e822548f7d741bd5619f8dced591
Monica Bui <monicabbui@gmail.com>
2021-03-02 18:19:50 -0800

    Resize show/hide window

--- client/src/components/ReviewAddFormModal/index.jsx
+++ client/src/components/ReviewAddFormModal/index.jsx
@@ -3,25 +3,14 @@ import ReviewsAddForm from '../ReviewsAddForm';
 import { FaTimesCircle } from 'react-icons/fa'
 import styles from './ReviewAddFormModal.css';
 
-class ReviewAddFormModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // displayModal: false
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <FaTimesCircle className={styles.closeModalButton} onClick={this.props.closeAddReviewModal} />
-        <div className={styles.modalWrapper} style={{opacity: this.props.displayModal ? '1' : '0'}} >
-          <ReviewsAddForm className={styles.modalContent} addReview={this.props.addReview} />
-        </div>
-        <button className={styles.closeModalButton} onClick={this.props.closeAddReviewModal}>Cancel</button>
-      </div>
-    );
-  }
-}
+const ReviewAddFormModal = (props) => (
+  <div>
+    <FaTimesCircle className={styles.closeModalButton} onClick={props.closeAddReviewModal} />
+    <div className={styles.modalWrapper} style={{ opacity: props.displayModal ? '1' : '0' }}>
+      <ReviewsAddForm className={styles.modalContent} addReview={props.addReview} />
+    </div>
+    <button type="button" className={styles.closeModalButton} onClick={props.closeAddReviewModal}>Cancel</button>
+  </div>
+);
 
 export default ReviewAddFormModal;
