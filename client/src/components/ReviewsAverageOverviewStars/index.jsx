9eb26e341226b8388360b24cf99b2994124904b1
Monica Bui <monicabbui@gmail.com>
2021-03-03 13:26:22 -0800

    Successfully implemented math to account for 1/4 and 3/4 stars

--- client/src/components/ReviewsAverageOverviewStars/index.jsx
+++ client/src/components/ReviewsAverageOverviewStars/index.jsx
@@ -4,12 +4,27 @@ import { FaStar, FaRegStar, FaStarHalfAlt } from 'react-icons/fa';
 const ReviewsAverageOverviewStars = (props) => {
   const { ratings } = props;
   const solidStars = Math.floor(ratings);
-  let halfAndRemainingStars;
+  const starDifference = ratings - solidStars;
+  let partialStars;
   const remainingStars = 4 - solidStars;
-  if (solidStars !== ratings) {
-    halfAndRemainingStars = <FaStarHalfAlt size={25} />;
+  if (starDifference > 0 && starDifference <= 0.12) {
+    // Unfilled star
+    partialStars = <FaRegStar size={25} />;
+  } else if (starDifference > 0.12 && starDifference <= 0.37) {
+    // Quarter filled star
+    partialStars = <FaRegStar size={5} />;
+  } else if (starDifference > 0.37 && starDifference <= 0.62) {
+    // Half filled star
+    partialStars = <FaStarHalfAlt size={25} />;
+  } else if (starDifference > 0.62 && starDifference <= 0.87) {
+    // 3/4 filled star
+    partialStars = <FaStarHalfAlt size={75} />;
+  } else if (starDifference > 0.87 && starDifference < 1) {
+    // Solid star
+    partialStars = <FaStar size={25} />;
   } else {
-    halfAndRemainingStars = <FaRegStar size={25} />;
+    // Unfilled star
+    partialStars = <FaRegStar size={25} />;
   }
 
   return (
@@ -17,7 +32,7 @@ const ReviewsAverageOverviewStars = (props) => {
       {
         [...Array(solidStars)].map(() => <FaStar size={25} />)
       }
-      {halfAndRemainingStars}
+      {partialStars}
       {
         [...Array(remainingStars)].map(() => <FaRegStar size={25} />)
       }
@@ -26,3 +41,29 @@ const ReviewsAverageOverviewStars = (props) => {
 };
 
 export default ReviewsAverageOverviewStars;
+
+// Notes and experiments:
+
+// const ReviewsAverageOverviewStars = (props) => {
+//   const { ratings } = props;
+//   const solidStars = Math.floor(ratings);
+//   let halfAndRemainingStars;
+//   const remainingStars = 4 - solidStars;
+//   if (solidStars !== ratings) {
+//     halfAndRemainingStars = <FaStarHalfAlt size={25} />;
+//   } else {
+//     halfAndRemainingStars = <FaRegStar size={25} />;
+//   }
+
+//   return (
+//     <div>
+//       {
+//         [...Array(solidStars)].map(() => <FaStar size={25} />)
+//       }
+//       {halfAndRemainingStars}
+//       {
+//         [...Array(remainingStars)].map(() => <FaRegStar size={25} />)
+//       }
+//     </div>
+//   );
+// };
