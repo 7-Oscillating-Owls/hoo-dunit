e6566c45de6fa17dd77e9615287ec9b844760c98
Monica Bui <monicabbui@gmail.com>
2021-03-02 18:00:25 -0800

    Implement close and cancel buttons

--- client/src/components/ReviewAddFormModal/index.jsx
+++ client/src/components/ReviewAddFormModal/index.jsx
@@ -12,13 +12,12 @@ class ReviewAddFormModal extends React.Component {
 
   render() {
     return (
-      <div className={styles.modalWrapper} style={{opacity: this.props.displayModal ? '1' : '0'}} >
-        <div>
-          <span className={styles.closeModalButton} onClick={this.props.closeAddReviewModal}>x</span>
+      <div>
+        <span className={styles.xButton} onClick={this.props.closeAddReviewModal}>x</span>
+        <div className={styles.modalWrapper} style={{opacity: this.props.displayModal ? '1' : '0'}} >
+          <ReviewsAddForm className={styles.modalContent} addReview={this.props.addReview} />
         </div>
-        <div className={styles.modalContent}>
-          <ReviewsAddForm addReview={this.props.addReview} />
-        </div>
+        <button className={styles.closeModalButton} onClick={this.props.closeAddReviewModal}>Cancel</button>
       </div>
     );
   }
