8855b674a4651aed2ae44f027a7ffdddeab38a32
Monica Bui <monicabbui@gmail.com>
2021-03-04 10:49:40 -0800

    Fixed radio button show hide feature

--- client/src/components/ReviewsAverageOverviewStars/index.jsx
+++ client/src/components/ReviewsAverageOverviewStars/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaStar, FaRegStar, FaStarHalfAlt } from 'react-icons/fa';
+import styles from './ReviewsAverageOverviewStars.css';
+// import images from '/static/images/';
 
 const ReviewsAverageOverviewStars = (props) => {
   const { ratings } = props;
@@ -36,6 +38,8 @@ const ReviewsAverageOverviewStars = (props) => {
       {
         [...Array(remainingStars)].map(() => <FaRegStar size={25} />)
       }
+      {/* <FaRegStar size={25} className={styles.testStar} /> */}
+      {/* <img src={images.QuarterStar.png} alt="Quarter Star" /> */}
     </div>
   );
 };
