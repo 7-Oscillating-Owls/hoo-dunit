45ff3eb2dbc9e038482f10fadc8ca05f3978fbeb
Monica Bui <monicabbui@gmail.com>
2021-03-04 15:13:30 -0800

    Fully implemented quarter and three quarter star features

--- client/src/components/ReviewsAverageOverviewStars/index.jsx
+++ client/src/components/ReviewsAverageOverviewStars/index.jsx
@@ -1,45 +1,43 @@
 import React from 'react';
-import { FaStar, FaRegStar, FaStarHalfAlt } from 'react-icons/fa';
 import styles from './ReviewsAverageOverviewStars.css';
-// import images from '/static/images/';
+// import images from '/static/images';
 
 const ReviewsAverageOverviewStars = (props) => {
-  const { ratings } = props;
+  // const { ratings } = props;
+  const ratings = 3.25;
   const solidStars = Math.floor(ratings);
   const starDifference = ratings - solidStars;
   let partialStars;
   const remainingStars = 4 - solidStars;
   if (starDifference > 0 && starDifference <= 0.12) {
     // Unfilled star
-    partialStars = <FaRegStar size={25} />;
+    partialStars = <img src="/static/images/UnfilledStar.png" className={styles.stars} alt="Unfilled Star" />;
   } else if (starDifference > 0.12 && starDifference <= 0.37) {
     // Quarter filled star
-    partialStars = <FaRegStar size={5} />;
+    partialStars = <img src="/static/images/QuarterStar.png" className={styles.stars} alt="Quarter Filled Star" />;
   } else if (starDifference > 0.37 && starDifference <= 0.62) {
     // Half filled star
-    partialStars = <FaStarHalfAlt size={25} />;
+    partialStars = <img src="/static/images/HalfStar.png" className={styles.stars} alt="Half Filled Star" />;
   } else if (starDifference > 0.62 && starDifference <= 0.87) {
     // 3/4 filled star
-    partialStars = <FaStarHalfAlt size={75} />;
+    partialStars = <img src="/static/images/ThreeQuarterStar.png" className={styles.stars} alt="Three Quarters Filled Star" />;
   } else if (starDifference > 0.87 && starDifference < 1) {
     // Solid star
-    partialStars = <FaStar size={25} />;
+    partialStars = <img src="/static/images/FilledStar.png" className={styles.stars} alt="Solid Star" />;
   } else {
     // Unfilled star
-    partialStars = <FaRegStar size={25} />;
+    partialStars = <img src="/static/images/FilledStar.png" className={styles.stars} alt="Solid Star" />;
   }
 
   return (
     <div>
       {
-        [...Array(solidStars)].map(() => <FaStar size={25} />)
+        [...Array(solidStars)].map(() => <img src="/static/images/FilledStar.png" className={styles.stars} alt="Solid Star" />)
       }
       {partialStars}
       {
-        [...Array(remainingStars)].map(() => <FaRegStar size={25} />)
+        [...Array(remainingStars)].map(() => <img src="/static/images/UnfilledStar.png" className={styles.stars} alt="Unfilled Star" />)
       }
-      {/* <FaRegStar size={25} className={styles.testStar} /> */}
-      {/* <img src={images.QuarterStar.png} alt="Quarter Star" /> */}
     </div>
   );
 };
