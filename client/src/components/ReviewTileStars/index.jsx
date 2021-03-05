a6b5500d0c144bbfddd2d675a753e014199420f5
Monica Bui <monicabbui@gmail.com>
2021-03-05 13:26:38 -0800

    Improve color scheme

--- client/src/components/ReviewTileStars/index.jsx
+++ client/src/components/ReviewTileStars/index.jsx
@@ -6,8 +6,8 @@ const ReviewTileStars = (props) => {
   const remainingStars = 5 - ratings;
   return (
     <div>
-      {[...Array(ratings)].map((star) => <FaStar size={20} color="#F5677B" key={star} />)}
-      {[...Array(remainingStars)].map((star) => <FaRegStar size={20} color="#F5677B" key={star} />)}
+      {[...Array(ratings)].map((star) => <FaStar size={20} color="#F5618E" key={star} />)}
+      {[...Array(remainingStars)].map((star) => <FaRegStar size={20} color="#F5618E" key={star} />)}
     </div>
   );
 };
