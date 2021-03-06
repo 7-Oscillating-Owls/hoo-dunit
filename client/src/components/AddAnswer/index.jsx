ab6408e4f7e27619245bc3ebb1c9af72da2ab43a
Ankur Tandan <tandan@gmail.com>
2021-03-06 14:20:45 -0800

    converted AddAnswer to class component

--- client/src/components/AddAnswer/index.jsx
+++ client/src/components/AddAnswer/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import styles from './AddAnswer.css';
 
-const AddAnswer = () => (
-  <small className={styles.AddAnswer}><a>Add Answer</a></small>
-);
+
+class AddAnswer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showAnswerModal: false
+    }
+  }
+
+  render() {
+    return (
+      <small className={styles.AddAnswer}><a>Add Answer</a></small>
+    )
+  }
+}
+
+// const AddAnswer = () => (
+//   <small className={styles.AddAnswer}><a>Add Answer</a></small>
+// );
 
 export default AddAnswer;
 
