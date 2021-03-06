b8991dc13786eb8161f091e61adc155cf07c15b2
Phillip Ho <philasan@gmail.com>
2021-03-05 20:44:30 -0800

    add client side routing

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,33 +1,63 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import RelatedProducts from '../RelatedProducts';
 import QuestionsAndAnswers from '../QuestionsAndAnswers';
 import Overview from '../Overview';
 import ReviewsList from '../ReviewsList';
 import styles from './App.css';
 
+class AppComponent extends React.Component {
+  componentDidMount() {
+    const { productId } = this.props.match.params;
+    console.log('initial fetch for ', productId);
+  }
+
+  componentDidUpdate() {
+    const { productId } = this.props.match.params;
+    console.log('fetch data for ', productId);
+  }
+
+  render() {
+    const { productId } = this.props.match.params;
+    return (
+      <>
+        <Overview />
+        <RelatedProducts productId={productId} />
+        <ReviewsList />
+        <QuestionsAndAnswers />
+      </>
+    );
+  }
+}
+
 const App = () => (
-  <div className={styles.app}>
+  <Router>
+    <div className={styles.app}>
+      <nav className={styles.nav}>
+        <h1 className={styles.brandName}>Oscillating Owls</h1>
+        <form name="appSearch" className={styles.searchForm}>
+          <input type="search" className={styles.searchInput} />
+        </form>
+      </nav>
+      <h3 className={styles.announcement}>
+        <span className={styles.announcementAlert}>site-wide accouncement message!</span>
+        <span> — sale / discount </span>
+        <span className={styles.announcementOffer}>offer</span>
+        <span> — </span>
+        <span className={styles.announcementHighlight}>new product highlight</span>
+      </h3>
 
-    <nav className={styles.nav}>
-      <h1 className={styles.brandName}>Oscillating Owls</h1>
-      <form name="appSearch" className={styles.searchForm}>
-        <input type="search" className={styles.searchInput} />
-      </form>
-    </nav>
-    <h3 className={styles.announcement}>
-      <span className={styles.announcementAlert}>site-wide accouncement message!</span>
-      <span> — sale / discount </span>
-      <span className={styles.announcementOffer}>offer</span>
-      <span> — </span>
-      <span className={styles.announcementHighlight}>new product highlight</span>
-    </h3>
-
-    <Overview />
-    <RelatedProducts />
-    <ReviewsList />
-    <QuestionsAndAnswers />
-
-  </div>
+      <Switch>
+        <Route path="/products/:productId" component={AppComponent} />
+        <Route path="*" render={() => (<div>No Route</div>)} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
