1d8847b1c7e33803b489cd68dacae104d9d27e66
Ankur Tandan <tandan@gmail.com>
2021-03-07 19:56:15 -0800

    cleaned up code in Answerlist component

--- client/src/components/Answerlist/index.jsx
+++ client/src/components/Answerlist/index.jsx
@@ -1,44 +1,39 @@
 import React from 'react';
 import styles from './Answerlist.css';
 import Answer from '../Answer';
-const axios = require ('axios');
+
+const axios = require('axios');
 
 class Answerlist extends React.Component {
   constructor(props) {
-    super(props)
-    this.state ={
-      answers: []
-    }
+    super(props);
+    this.state = {
+      answers: [],
+    };
   }
 
   componentDidMount() {
     axios.get('/answers')
       .then(response => {
-        this.setState({ answers: response.data})
+        this.setState({ answers: response.data });
       });
   }
 
   render() {
-
     let answercomponents;
     if (this.state.answers['results']) {
-
       answercomponents = this.state.answers['results'].slice(0, 2).map((answer) => {
-
         return (
           <Answer answer={answer} key={answer.answer_id} />
         )
       });
-
     }
     return (
       <div className={styles.answerlist}>
         {answercomponents}
       </div>
-    )
-
+    );
   }
 }
-
 
 export default Answerlist;
