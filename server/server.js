e0918b46ccc12ca47b64d2faf2b4574fc2412577
Ankur Tandan <tandan@gmail.com>
2021-03-08 13:51:26 -0800

    implmeneted post request for answer

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.get('/qa', (request, response) => {
 });
 
 app.get('/answers', (request, response) => {
+  const { questionId } = this.props;
   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions/84310/answers', {
     headers: {
       'Authorization' : token
@@ -113,6 +114,29 @@ app.get('/api/products/:productId/related', (request, response) => {
     .catch((error) => response.status(400).send(error));
 });
 
+app.post('/qa/postAnswer', (req, res) => {
+  const {
+    body,
+    name,
+    email,
+    questionId
+  } = req.body;
+
+  const { questionId }  = this.props;
+  axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions/{questionId}/answers`, {
+    headers: {
+      'Authorization' : token
+    };
+  })
+    .then((res) => {
+      response.send('Successfuly posted');
+    })
+    .catch((error) => {
+      response.send('error posting');
+    });
+})
+
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
+
