e66cb75948881c1b30875549bc98437fbc48dcbd
Ankur Tandan <tandan@gmail.com>
2021-03-07 20:34:45 -0800

    implemented test for QuestionModal component

--- client/src/components/QuestionModal/QuestionModal.test.js
+++ client/src/components/QuestionModal/QuestionModal.test.js
@@ -1,9 +1,9 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import Answerlist from '.';
+import QuestionModal from '.';
 
 it('should exist', () => {
-  const wrapper = shallow(<Answerlist />);
+  const wrapper = shallow(<QuestionModal />);
   expect(wrapper.exists()).toBe(true);
 });
