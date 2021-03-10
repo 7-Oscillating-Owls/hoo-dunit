b4a63ede1ab02642681516bfe2e8e411fb7d9898
Monica Bui <monicabbui@gmail.com>
2021-03-10 11:22:55 -0800

    Will work on tests in test branch

--- client/src/components/ReviewAddFormModal/ReviewAddFormModal.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-import ReviewAddFormModal from '.';
-
-test('ReviewAddFormModal should exist', () => {
-  const wrapper = shallow(<ReviewAddFormModal />);
-  expect(wrapper.exists()).toBe(true);
-});
