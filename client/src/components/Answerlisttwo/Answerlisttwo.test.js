b616d7f522f8d30a2043c38ff0d93bbbd3c5a6ca
Ankur Tandan <tandan@gmail.com>
2021-03-11 15:36:16 -0800

    implmeneted additional tests

--- client/src/components/Answerlisttwo/Answerlisttwo.test.js
+++ client/src/components/Answerlisttwo/Answerlisttwo.test.js
@@ -1,5 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import Answer from '../Answer';
+import LoadMoreAnswers from '../LoadMoreAnswers'
 import dummyAnswers from '../../../../data/dummyDataAnswers';
 
 import Answerlisttwo from '.';
@@ -13,3 +15,18 @@ it('should exist', () => {
   const wrapper = shallow(<Answerlisttwo answer={dummyAnswers} />);
   expect(wrapper.exists()).toBe(true);
 });
+
+
+// it('should exist', () => {
+//   const wrapper = shallow(<Answer answer={dummyAnswers} />);
+//   expect(wrapper.exists()).toBe(true);
+// });
+
+// it('should exist', () => {
+//   const wrapper = shallow(<LoadMoreAnswers click={dummyAnswers} />);
+//   expect(wrapper.exists()).toBe(true);
+// });
+// it('should display the answer text', () => {
+//   const wrapper = shallow(<Answerlisttwo answer={dummyAnswers.results[0]} />);
+//   expect(wrapper.find('.answer').text()).toContain('Quia dolor cum eligendi rem veniam ut dolorem similique.');
+// });
